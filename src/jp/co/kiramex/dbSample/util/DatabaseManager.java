d155230bd44b3cd032e832437131627a208601f6
yrisa55 <shariko27@outlook.com>
2024-06-19 22:43:36 +0900

    テキストの内容

---
+++ src/jp/co/kiramex/dbSample/util/DatabaseManager.java
@@ -0,0 +1,40 @@
+package jp.co.kiramex.dbSample.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+   // データベース接続と結果取得のための変数
+    private static Connection con;
+
+    /*
+     *  データベース接続を行うメソッド
+     */
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        // 1. ドライバのクラスをJava上で読み込む
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 2. DBと接続する
+        con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true",
+                "root",
+                "pw06100!"
+                );
+
+        return con;
+    }
+
+    /*
+     * データベース接続を閉じるメソッド
+     */
+    public static void close() {
+        // 7.接続を閉じる
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
